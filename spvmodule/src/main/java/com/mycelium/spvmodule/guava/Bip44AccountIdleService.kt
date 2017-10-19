ae975656c51073119494c19b8961f2e8cf2aa434
Nelson MELINA <nelsonmelina@6inthemorning.com>
2017-10-19 15:41:19 +0100

    Make Bip44AccountIdleService return a list of bitlib TransactionSummary

--- spvmodule/src/main/java/com/mycelium/spvmodule/guava/Bip44AccountIdleService.kt
+++ spvmodule/src/main/java/com/mycelium/spvmodule/guava/Bip44AccountIdleService.kt
@@ -12,6 +12,7 @@ import android.support.v4.content.LocalBroadcastManager
 import android.text.format.DateUtils
 import android.util.Log
 import android.widget.Toast
+import com.google.common.base.Optional
 import com.google.common.collect.ImmutableList
 import com.google.common.util.concurrent.AbstractScheduledService
 import com.google.common.util.concurrent.ListenableFuture
@@ -21,6 +22,7 @@ import com.mycelium.modularizationtools.CommunicationManager
 import com.mycelium.spvmodule.*
 import com.mycelium.wapi.model.TransactionEx
 import com.mycelium.wapi.model.TransactionSummary
+import com.mycelium.wapi.wallet.currency.ExactBitcoinValue
 import org.bitcoinj.core.*
 import org.bitcoinj.core.Context.propagate
 import org.bitcoinj.core.listeners.DownloadProgressTracker
@@ -705,16 +707,17 @@ class Bip44AccountIdleService : AbstractScheduledService() {
         }
     }
 
-    fun getTransactionsSummary(accountIndex: Int) : Set<TransactionSummary> {
+    fun getTransactionsSummary(accountIndex: Int) : List<TransactionSummary> {
+        val transactionsSummary = mutableListOf<TransactionSummary>()
         val walletAccount = walletsAccountsMap.get(accountIndex)
-        val transactions = walletAccount.getTransactions(true)
+        val transactions = walletAccount!!.getTransactions(true)
         for (transactionBitcoinJ in transactions) {
             val transactionBitLib : com.mrd.bitlib.model.Transaction =
                     com.mrd.bitlib.model.Transaction.fromBytes(transactionBitcoinJ.bitcoinSerialize())
 
             transactionBitcoinJ.isAnyOutputSpent
             // Outputs
-            var satoshis: Long = 0
+            val satoshis: Long = 0
             val toAddresses = java.util.ArrayList<Address>()
             var destAddress: Address? = null
             val valueSentToMe = transactionBitcoinJ.getValueSentToMe(walletAccount)
@@ -723,7 +726,7 @@ class Bip44AccountIdleService : AbstractScheduledService() {
             }
 
             val networkParametersBitlib : NetworkParameters = {
-                when(walletAccount!!.networkParameters.id) {
+                when(walletAccount.networkParameters.id) {
                     org.bitcoinj.core.NetworkParameters.ID_MAINNET -> NetworkParameters.productionNetwork
                     org.bitcoinj.core.NetworkParameters.ID_TESTNET -> NetworkParameters.testNetwork
                     else -> {
@@ -743,30 +746,21 @@ class Bip44AccountIdleService : AbstractScheduledService() {
                     toAddresses.add(toAddress)
                 }
             }
+            val confirmations : Int = transactionBitcoinJ.confidence.depthInBlocks
+            val isQueuedOutgoing = transactionBitcoinJ.isPending
 
-            // Inputs
-            if (!transactionBitLib.isCoinbase()) {
-                for (input in transactionBitLib.inputs) {
-                    // find parent output
-                    val funding = _backing.getParentTransactionOutput(input.outPoint)
-                    if (funding == null) {
-                        _logger.logError("Unable to find parent output for: " + input.outPoint)
-                        continue
-                    }
-                    if (isMine(funding)) {
-                        satoshis -= funding!!.value
-                    }
-                }
+            val destAddressOptional = if(destAddress != null) {
+                Optional.of(destAddress)
+            } else {
+                Optional.absent()
             }
-
-
-
-
-
-
-
-            val transactionSummary : TransactionSummary = TransactionSummary(transactionBitcoinJ.hash)
+            transactionsSummary.add(TransactionSummary(transactionBitLib.hash,
+                    ExactBitcoinValue.from(transactionBitcoinJ.getValue(walletAccount).value),
+                    transactionBitcoinJ.getValueSentToMe(walletAccount).isPositive,
+                    transactionBitcoinJ.lockTime, transactionBitcoinJ.confidence.appearedAtChainHeight,
+                    confirmations, isQueuedOutgoing, null, destAddressOptional, toAddresses))
         }
+        return transactionsSummary.toList()
     }
 
     inner class DownloadProgressTrackerExt : DownloadProgressTracker() {
