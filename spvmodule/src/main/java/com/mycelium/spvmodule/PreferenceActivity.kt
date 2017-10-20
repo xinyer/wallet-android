d9b9eaac00f3a843c4bd8e0e5298b99585c75bd9
Tomasz Ludek <tomasz.ludek@gmail.com>
2017-10-20 17:41:51 +0200

    Removing debugging code

--- spvmodule/src/main/java/com/mycelium/spvmodule/PreferenceActivity.kt
+++ spvmodule/src/main/java/com/mycelium/spvmodule/PreferenceActivity.kt
@@ -17,19 +17,11 @@
 
 package com.mycelium.spvmodule
 
-import android.database.Cursor
-import android.net.Uri
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.TextUtils
 import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.ScrollView
-import com.mrd.bitlib.model.Address
-import com.mrd.bitlib.util.Sha256Hash
-import com.mycelium.spvmodule.providers.TransactionContract
-import com.mycelium.wapi.model.TransactionDetails
-import java.util.ArrayList
 
 class PreferenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,64 +29,11 @@ class PreferenceActivity : AppCompatActivity() {
         val sw = ScrollView(this)
         sw.id = 171415
         setContentView(sw, LayoutParams(MATCH_PARENT, MATCH_PARENT))
-        if( savedInstanceState == null) {
+        if (savedInstanceState == null) {
             val settingsFragment = SettingsFragment()
             val ft = supportFragmentManager.beginTransaction()
             ft.add(sw.id, settingsFragment)
             ft.commit()
         }
-        getTransactionDetails()
-    }
-
-    private fun getTransactionDetails(): TransactionDetails? {
-        var transactionDetails: TransactionDetails? = null
-        val uri = Uri.withAppendedPath(TransactionContract.TransactionDetails.CONTENT_URI("com.mycelium.spvmodule.test"), "bca6c7903fb4bdd5d796501a7f54bed618550dd17e4eb3c9ef2b43214ecc6dc1")
-        val selection = TransactionContract.TransactionDetails.SELECTION_ACCOUNT_INDEX
-        val accountIndex = 0;
-        val selectionArgs = arrayOf(Integer.toString(accountIndex))
-        var cursor: Cursor? = null
-        val contentResolver = contentResolver
-        try {
-            cursor = contentResolver.query(uri, null, selection, selectionArgs, null)
-            if (cursor != null) {
-                while (cursor.moveToNext()) {
-                    transactionDetails = from(cursor)
-                }
-            }
-        } finally {
-            if (cursor != null) {
-                cursor.close()
-            }
-        }
-        return transactionDetails
-    }
-
-    private fun from(cursor: Cursor): TransactionDetails {
-        val rawTxId = cursor.getString(cursor.getColumnIndex(TransactionContract.TransactionDetails._ID))
-        val hash = Sha256Hash.fromString(rawTxId)
-        val height = cursor.getInt(cursor.getColumnIndex(TransactionContract.TransactionDetails.HEIGHT))
-        val time = cursor.getInt(cursor.getColumnIndex(TransactionContract.TransactionDetails.TIME))
-        val rawSize = cursor.getInt(cursor.getColumnIndex(TransactionContract.TransactionDetails.RAW_SIZE))
-
-        val rawInputs = cursor.getString(cursor.getColumnIndex(TransactionContract.TransactionDetails.INPUTS))
-        val rawOutputs = cursor.getString(cursor.getColumnIndex(TransactionContract.TransactionDetails.OUTPUTS))
-
-        val inputs = extract(rawInputs)
-        val outputs = extract(rawOutputs)
-        return TransactionDetails(hash, height, time, inputs, outputs, rawSize)
-    }
-
-    private fun extract(data: String): Array<TransactionDetails.Item> {
-        val result = ArrayList<TransactionDetails.Item>()
-        if (!TextUtils.isEmpty(data)) {
-            val dataParts = data.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            for (input in dataParts) {
-                val inputParts = input.split(" BTC".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val value = java.lang.Long.valueOf(inputParts[0])!!
-                val address = Address.fromString(inputParts[1])
-                result.add(TransactionDetails.Item(address, value, false))
-            }
-        }
-        return result.toTypedArray()
     }
 }
