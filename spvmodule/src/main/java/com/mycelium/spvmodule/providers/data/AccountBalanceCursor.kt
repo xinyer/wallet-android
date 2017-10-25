e34e99d4aaa7c81e04356ba3484decc8733bdfee
Nelson MELINA <nelson.melina@mycelium.com>
2017-10-25 11:36:21 +0100

    Revert "making it compile"
    This reverts commit 236a4d4bf2ecaf0049b4763c0c8e3fe72bb219aa.

--- spvmodule/src/main/java/com/mycelium/spvmodule/providers/data/AccountBalanceCursor.kt
+++ spvmodule/src/main/java/com/mycelium/spvmodule/providers/data/AccountBalanceCursor.kt
@@ -3,6 +3,12 @@ package com.mycelium.spvmodule.providers.data
 import android.database.MatrixCursor
 import com.mycelium.spvmodule.providers.TransactionContract
 
-/* class AccountBalanceCursor: MatrixCursor(arrayOf(TransactionContract.AccountBalance._ID,
-        TransactionContract.AccountBalance.BALANCE), 1)
-*/
+/**
+ * Created by Nelson on 17/10/2017.
+ */
+class AccountBalanceCursor()
+    : MatrixCursor(arrayOf(TransactionContract.AccountBalance._ID,
+        TransactionContract.AccountBalance.BALANCE),
+        1) {
+
+}
